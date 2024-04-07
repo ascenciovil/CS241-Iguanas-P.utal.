ced6c5333dd1a7bda3f793b9c37f03ab7b731154
MrFalric <110941527+MrFalric@users.noreply.github.com>
2024-04-07 18:28:20 -0400

    Avances almacenamiento de info
    Felipe Calderón Dedique 2:47 para estudio de Vue.js y Supabase, y las otras 2:13 para implementar este avance
    Ennzo Ortiz Dedique 2:53 para estudio de Vue.js y Supabase y las otras 2:07 para implementar este avance

---
+++ main.js
@@ -0,0 +1,15 @@
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = 'https://jaubayxbwflblrthehdj.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphdWJheXhid2ZsYmxydGhlaGRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI0MzM1MDIsImV4cCI6MjAyODAwOTUwMn0.tGggr751oq5X9NZ3PVGcv5jnBiQOicsbBZPViOHMtYI';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const app = createApp(App);
+
+// Haciendo supabase accesible en todos los componentes
+app.config.globalProperties.$supabase = supabase;
+
+app.mount('#app');
