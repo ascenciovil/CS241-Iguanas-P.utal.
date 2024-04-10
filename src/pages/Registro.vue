99a66d944fdf792383bb7d8b528b375e5408c415
Constru Software <hvasquez21@alumnos.utalca.cl>
2024-04-10 01:16:12 -0400

    Interfaz registro usuario

--- src/pages/Registro.vue
+++ src/pages/Registro.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-    <h1>Agregar Usuario</h1>
-    <form @submit.prevent="createAccount">
-      <label for="email">Email: </label>
-      <input type="email" id= "email" v-model="email" placeholder="Email" />
-      <br>
-      <label for ="password"> Password: </label>
-      <input type="password" id= "password" v-model="password" placeholder="contraseña" />
-      <br>
-      <button type="submit">Añadir Usuario</button>
-    </form>
-  </div>
+  <body id="app">
+        <h1>Agregar Usuario</h1>
+        <form @submit.prevent="createAccount">
+            <div class="email">
+                <label for="email" class="emailLabel">Email:</label>
+                <input type="email" id="email" v-model="email" required> 
+            </div>
+            <div class="password">
+                <label for="password" class="passwordLabel">Password:</label>
+                <input type="password" id="password" v-model="password" required>
+            </div>
+            <button type="submit" class="boton">Añadir Usuario</button>
+        </form>
+    </body>
 </template>
 
 <script setup>
@@ -43,6 +45,56 @@ async function createAccount() {
 }
 </script>
 
-<style>
-/* Estilos opcionales */
+<style scoped>
+/* Add your custom styles here */
+body{
+    background-color: #ebdeff;
+    height: 100%;
+    padding-bottom: 2%;
+    width: 100%;
+}
+h1{
+    text-align: center;
+    background-color: #ff7f27;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    margin-bottom: 2%;
+}
+form{
+    position: relative;
+    left: 60%;
+    width: 40%;
+    height: 300px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    background-color: #00cccc;
+    align-items: center;
+}
+.email{
+    padding-top: 10%;
+    padding-bottom: 4%;
+    position: relative;
+}
+.password{
+    padding-top: 4%;
+    padding-bottom: 4%;
+    position: relative;
+}
+.boton{
+    margin-top: 4%;
+    margin-bottom: 10%;
+    margin-left: 45%;
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    position: absolute;
+}
+div{
+    display: flex;
+    justify-content: space-between;
+    width: 50%;
+    margin-left: 25%;
+    height: 8%;
+}
 </style>
