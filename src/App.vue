7c27053ec97af180ddb378b978eaad74a33af066
PedroRetamal <111097062+PedroRetamal@users.noreply.github.com>
2024-04-19 23:08:41 -0400

    Pedro Retamal: Identificar rol de usuario para que al loguearse se muestre lo que debe ver ese rol, mejoras al login y registro (reconoce bien ahora a profesor y estudiante) además de crear un MainLayout para determinar que se vé y que no

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['loginEstudiante','loginProfesor','loginAux']);
+</script>
+
 <template>
   <h1>Propuestas Utalca</h1>
 
   <nav>
-    <RouterLink to="/login">Login</RouterLink> |
-    <RouterLink to="/Registro">Registro</RouterLink> |
-    <RouterLink to="/Editar">EditarPerfil</RouterLink> |
-    <RouterLink to="/Propuesta">Propuesta</RouterLink>
+    <span v-if="loginAux">
+      <RouterLink to="/login">Login</RouterLink> |
+      <RouterLink to="/Registro">Registro</RouterLink>
+    </span>
+    <span v-if="loginEstudiante">
+      <RouterLink to="/Editar">EditarPerfil</RouterLink> |
+      <RouterLink to="/Propuesta">Propuesta</RouterLink>
+      <div><button @click="logout">Log Out</button>
+        <button @click="Conectado">Ver usuario</button></div>
+      
+      
+    </span>
+    <span v-if="loginProfesor">
+      <RouterLink to="/Editar">EditarPerfil</RouterLink>
+      <div><button @click="logout">Log Out</button>
+        <button @click="Conectado">Ver usuario</button></div>
+    </span>
     <div class="dropdown">
       <button class="acercaDe">Acerca de</button>
       <div class="dropdown-content">
@@ -18,85 +37,119 @@
   <main>
     <RouterView />
   </main>
-  <body>
-    <div id="ventanaLineamientos" class="ventana">
-      <div class="contenido">
-        <h2>Lineamientos</h2>
-        <p>1° Registrarse</p>
-        <br>
-        <p>2° Iniciar sesión</p>
-        <br>
-        <p>3° Irse a la página de propuestas</p>
-        <br>
-        <p>4° Ingresar título y propuesta</p>
-        <button @click="cerrarLineamientos()">Cerrar</button>
-      </div>
+  <div id="ventanaLineamientos" class="ventana">
+    <div class="contenido">
+      <h2>Lineamientos</h2>
+      <p>1° Registrarse</p>
+      <br>
+      <p>2° Iniciar sesión</p>
+      <br>
+      <p>3° Irse a la página de propuestas</p>
+      <br>
+      <p>4° Ingresar título y propuesta</p>
+      <button @click="cerrarLineamientos()">Cerrar</button>
     </div>
-    <div id="ventanaDocumentacion" class="ventana">
-      <div class="contenido">
-        <h2>Documentacion</h2>
-        <p>Deben cumplir con las leyes del país, no involucrar datos personales ni ocupar propiedad de otros autores.</p>
-        <button @click="cerrarDocumentacion()">Cerrar</button>
-      </div>
+  </div>
+  <div id="ventanaDocumentacion" class="ventana">
+    <div class="contenido">
+      <h2>Documentacion</h2>
+      <p>Deben cumplir con las leyes del país, no involucrar datos personales ni ocupar propiedad de otros autores.</p>
+      <button @click="cerrarDocumentacion()">Cerrar</button>
     </div>
-    <div id="ventanaPreguntas" class="ventana">
-      <div class="contenido">
-        <h2>Preguntas Frecuentes</h2>
-        <p>Página exclusivamente dirigida a propuestas de los estudiantes para los estudiantes</p>
-        <br>
-        <p>Las propuestas se revisan en un máximo de una semana</p>
-        <br>
-        <p>Puedes enviar propuestas solo en tu campus, pero puedes observar las establecidas en los otros</p>
-        <br>
-        <p>Contacto: jascencio21@alumnos.utalca.cl</p>
-        <button @click="cerrarPreguntas()">Cerrar</button>
-      </div>
+  </div>
+  <div id="ventanaPreguntas" class="ventana">
+    <div class="contenido">
+      <h2>Preguntas Frecuentes</h2>
+      <p>Página exclusivamente dirigida a propuestas de los estudiantes para los estudiantes</p>
+      <br>
+      <p>Las propuestas se revisan en un máximo de una semana</p>
+      <br>
+      <p>Puedes enviar propuestas solo en tu campus, pero puedes observar las establecidas en los otros</p>
+      <br>
+      <p>Contacto: jascencio21@alumnos.utalca.cl</p>
+      <button @click="cerrarPreguntas()">Cerrar</button>
     </div>
-  </body>
+  </div>
 </template>
 
-<script setup>
+<script>
+import { supabase } from "./clients/supabase";
+import { ref } from 'vue';
+
+// Definir loggedIn como ref
+const loginEstudiante = ref(false);
+const loginProfesor = ref(false);
+const loginAux=ref(true);
+// Función para actualizar el estado de loggedIn
+function updateLoginState(valueEstudiante,valueProfesor) {
+    loginEstudiante.value = valueEstudiante;
+    loginProfesor.value = valueProfesor;
+    if(loginAux.value){
+      loginAux.value=false;
+    }else{
+      loginAux.value=true;
+    }
+    console.log(loginAux.value);
+}
+export { loginEstudiante,loginProfesor, updateLoginState };
+// Funciones de ventana
 function abrirLineamientos() {
   var elemento = document.getElementById("ventanaLineamientos");
   if (elemento != null) {
     elemento.style.display = "block";
   }
-};
-
+}
+async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.error("Error al cerrar sesión:", error.message);
+  } else {
+    console.log("Sesión cerrada exitosamente.");
+    updateLoginState(false);
+  }
+}
+async function Conectado() {
+  const { user, error } = await supabase.auth.getSession();
+  if (error) {
+    console.error("Error al obtener la sesión:", error.message);
+  } else {
+    console.log("Usuario en sesión:", user);
+  }
+}
 function cerrarLineamientos() {
   var elemento = document.getElementById("ventanaLineamientos");
   if (elemento != null) {
     elemento.style.display = "none";
   }
-};
+}
 
 function abrirDocumentacion() {
   var elemento = document.getElementById("ventanaDocumentacion");
   if (elemento != null) {
     elemento.style.display = "block";
   }
-};
+}
 
 function cerrarDocumentacion() {
   var elemento = document.getElementById("ventanaDocumentacion");
   if (elemento != null) {
     elemento.style.display = "none";
   }
-};
+}
 
 function abrirPreguntas() {
   var elemento = document.getElementById("ventanaPreguntas");
   if (elemento != null) {
     elemento.style.display = "block";
   }
-};
+}
 
 function cerrarPreguntas() {
   var elemento = document.getElementById("ventanaPreguntas");
   if (elemento != null) {
     elemento.style.display = "none";
   }
-};
+}
 </script>
 
 <style scoped>
---
+++ src/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <App :loginEstudiante="loginEstudiante":loginProfesor="loginProfesor":loginAux="loginAux" />
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const loginEstudiante = ref(false); // O cualquier valor por defecto
+  const loginProfesor = ref(false);
+  const loginAux = ref(true);
+  </script>
